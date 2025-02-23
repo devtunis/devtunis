775e395f7f96c2daa9d228bcf08e31bbb605e45c
Ghaith Nahdi <121437369+TUNISIA-user@users.noreply.github.com>
2025-02-23 20:14:48 +0100

    Create problemSOlveing.py
    n = 6
    start = [0, 1, 3, 5, 5, 8]
    end = [6, 2, 4, 9, 7, 9]
    
     
    i = 0
    metting  =  1
    while i  < n-1:
        s = start[i+1]
        e = end [i] # i think here will update auto ?a
         
        if s>=e:
            metting = metting +1
            
           
            
        i+=1
    print(metting)
    
    # this my solution its so good ahhah a i love my sef
    # DO THIS SUITE FOR THIS WOKRING AS CLASS

---
+++ problemSOlveing.py
@@ -0,0 +1,21 @@
+n = 6
+start = [0, 1, 3, 5, 5, 8]
+end = [6, 2, 4, 9, 7, 9]
+
+ 
+i = 0
+metting  =  1
+while i  < n-1:
+    s = start[i+1]
+    e = end [i] # i think here will update auto ?a
+     
+    if s>=e:
+        metting = metting +1
+        
+       
+        
+    i+=1
+print(metting)
+
+# this my solution its so good ahhah a i love my sef
+# DO THIS SUITE FOR THIS WOKRING AS CLASS 
